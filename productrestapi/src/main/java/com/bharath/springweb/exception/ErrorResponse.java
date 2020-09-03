dccc05e00586d467fa3a0df3342dbe34d9e6df22
shi514652697 <ssnegi_4all@yahoo.com>
2020-09-02 21:16:54 -0500

    added

--- productrestapi/src/main/java/com/bharath/springweb/exception/ErrorResponse.java
+++ productrestapi/src/main/java/com/bharath/springweb/exception/ErrorResponse.java
@@ -2,22 +2,32 @@ package com.bharath.springweb.exception;
 
 import org.springframework.http.HttpStatus;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
 public class ErrorResponse {
-	
+
 	private HttpStatus status;
-	private String message;
-	public HttpStatus getStatus() {
+
+	 public HttpStatus getStatus() {
 		return status;
 	}
+
 	public void setStatus(HttpStatus status) {
 		this.status = status;
 	}
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
+
+	private String message;
+
 	public ErrorResponse(HttpStatus status, String message) {
 		
 		this.status = status;
